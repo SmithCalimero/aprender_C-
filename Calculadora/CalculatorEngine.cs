de4ae1dc43528bfc20be605cd39bfc9c8a33061d
SmithCalimero <59543526+SmithCalimero@users.noreply.github.com>
2022-08-29 16:45:03 +0100

    Projeto Calculadora

---
+++ Calculadora/CalculatorEngine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class CalculatorEngine
+    {
+        //vamos executar aqui a lógica das operações
+        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+            double result;
+
+            switch (argOperation.ToLower())
+            {
+                //adição
+                case "add":
+                case "+":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+                //subtração
+                case "subtract":
+                case "-":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+                //multiplicação
+                case "multiply":
+                case "*":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+                //divisão
+                case "divide":
+                case "/":
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+                //em caso de não ser reconhecido o input = não ser um dos casos
+                default:
+                    throw new InvalidOperationException("Specified operation is not recognized"); 
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                //INPUTS - É utilizado uma classe própria para os converter
+                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                string operation = Console.ReadLine();
+
+                //usamos uma classe própria para executar a lógica
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine(result);
+
+            } catch (Exception ex)
+            {
+                //NUM CASO REAL SERIA BOM DAR LOG A ESTA MENSAGEM!!!!!!
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
+
